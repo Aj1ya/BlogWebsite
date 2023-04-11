302139500d804f26a3f516a4e3a818edabbb32a6
Ajaya Maharjan <ajayamaharjan93@gmail.com>
2023-04-12 07:36:02 +1000

    Update: Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # BlogWebsite
 Blog website built using ejs, js , html and css
+
+
+version 1.0 
+
+# Intial commit
+- initial commit
---
+++ app.js
@@ -0,0 +1,28 @@
+// jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const homeStartingContent =  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum incidunt quaerat, doloribus dolorum voluptatibus perspiciatis dolore ex reprehenderit tempora omnis. Ad facilis perferendis, illo consequatur repudiandae dolorum animi minus quod?"
+const aboutContent =  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum incidunt quaerat, doloribus dolorum voluptatibus perspiciatis dolore ex reprehenderit tempora omnis. Ad facilis perferendis, illo consequatur repudiandae dolorum animi minus quod?"
+const contactContent =  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum incidunt quaerat, doloribus dolorum voluptatibus perspiciatis dolore ex reprehenderit tempora omnis. Ad facilis perferendis, illo consequatur repudiandae dolorum animi minus quod?"
+
+const app = express();
+
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/", (req, res)=>{
+    res.render("home", {homeContent: homeStartingContent});
+})
+
+app.post("/", (req, res)=>{
+    console.log("home");
+})
+
+app.listen(3000, (req, res)=>{
+    console.log("app started in 3000");
+})
